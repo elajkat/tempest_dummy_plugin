6c1aa15d7eb3c0805e13df2ff2e75de95f877075
Lajos Katona <lajos.katona@ericsson.com>
2015-09-15 07:28:59 +0200

    How to info ro README, and config example
    Extend README with usage info, and config options
    Extend the dummy testcase with some config option example
    
    Change-Id: I41120f7c6fa3406731c5484d206585c3dc237ec0

--- README.rst
+++ README.rst
@@ -5,3 +5,21 @@ This is a demonstarion for how to write a tempest test plugin.
 This feature is available from tag 6 in tempest, see.:
 http://docs.openstack.org/developer/tempest/plugin.html
 
+How to use
+----------
+An example how I used it:
+
+- In tempest repo install a venv for tempest::
+  $ tox -evenv
+- Activate the venv and install the tempest_dummy_plugin to the venve::
+  $ . .tox/venv/bin/activate
+  (venv)$ pyton setup.py install
+- You can check the test listing to see if the tests are detected::
+  $ testr list-tests | grep dummy
+
+Configuration
+-------------
+Add the following lines to the end of tempest.conf::
+  [dummy_config_options]
+  funny_cfg_option = funny
+And you can reach the options in the old way from your testcases.
--- dummyplugin/tests/api/network/test_dummy_net_feature.py
+++ dummyplugin/tests/api/network/test_dummy_net_feature.py
@@ -1,9 +1,24 @@
 from tempest.api.network import base
+from tempest import config
 from tempest import test
+
+CONF = config.CONF
 
 
 class DummyFeatureTestJSON(base.BaseNetworkTest):
     @test.attr(type='smoke')
     @test.idempotent_id('8e03f417-47e9-4470-beff-0814bfb867d3')
     def test_create_update_delete_one_dummy(self):
-        self.assertEqual(2, 2)
+        if CONF.dummy_config_options.funny_cfg_option == 'funny':
+            self.assertEqual(
+                2, 2,
+                'In the config there is {d_cfg}, so 2 equals with2.'.format(
+                    d_cfg=CONF.dummy_config_options.funny_cfg_option))
+        elif CONF.dummy_config_options.funny_cfg_option == 'not_funny':
+            msg = 'In the config there is {d_cfg}, so 2 not equals with 3.'
+            self.assertEqual(
+                2, 3,
+                msg.format(
+                    d_cfg=CONF.dummy_config_options.funny_cfg_option))
+        else:
+            self.assertEqual(42, 42)
